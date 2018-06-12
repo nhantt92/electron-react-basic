532475f789b01e9174c30ec2ef5fc2facb60cfd0
nhantt <trungnhan@phuonghai.com>
2018-06-12 16:45:49 +0700

    upload

---
+++ main_process.js
@@ -0,0 +1,14 @@
+const electron = require('electron')
+const {app, BrowserWindow} = electron
+
+require('electron-reload')(__dirname)
+
+let mainWindow
+
+app.on('ready', () => {
+
+    mainWindow = new BrowserWindow({width: 800, height: 600})
+
+    mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+
+})
